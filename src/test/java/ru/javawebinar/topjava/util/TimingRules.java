7dc8098a40fa69d251d9294039a28c2c2b2a3718
cnsler <cnsler@gmail.com>
2022-11-01 17:58:02 +0200

    HW4 add tests execution time to MealServiceTest

---
+++ src/test/java/ru/javawebinar/topjava/util/TimingRules.java
@@ -0,0 +1,56 @@
+/**
+ * https://korelyakov.com/blog/how-to-add-execution-time-in-junit-tests/
+ */
+
+package ru.javawebinar.topjava.util;
+
+import org.junit.rules.ExternalResource;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+public class TimingRules {
+    private static final Logger log = LoggerFactory.getLogger("result");
+
+    private static final StringBuilder results = new StringBuilder();
+
+    public static final Stopwatch STOPWATCH = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            String result = String.format("%-95s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
+            results.append(result).append('\n');
+            log.info(result + " ms\n");
+        }
+    };
+
+    // элемент разметки
+    //    https://dzone.com/articles/applying-new-jdk-11-string-methods
+    //private static final String DELIM = "-".repeat(103);
+
+    //    https://stackoverflow.com/questions/1235179/simple-way-to-repeat-a-string
+    // previous String repeat for Java 11
+    // in Java 8:
+    private static final String DELIM = String.join("", Collections.nCopies(103, "-"));
+
+    //форматируем красивый вывод в консоль
+    public static final ExternalResource SUMMARY = new ExternalResource() {
+
+        //обнуляем перед запуском тестов класса
+        @Override
+        protected void before() throws Throwable {
+            results.setLength(0);
+        }
+
+        //выводим отформатированный результат
+        @Override
+        protected void after() {
+            log.info("\n" + DELIM +
+                    "\nTest                                                                                       Duration, ms" +
+                    "\n" + DELIM + "\n" + results + DELIM + "\n");
+        }
+    };
+}
