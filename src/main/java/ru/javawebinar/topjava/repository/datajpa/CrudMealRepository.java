35641201db12d6d0d22ed14249b930ffeb152627
cnsler <cnsler@gmail.com>
2022-11-08 13:16:49 +0200

    HW5 implement JpaRepository methods except save()

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,17 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    Meal findMealByIdAndUserId(int id, int userId);
+
+    List<Meal> findMealsByUserId(Sort sort, int userId);
+
+    List<Meal> findMealsByUserIdAndDateTimeAfterAndDateTimeBefore(Sort sort, int userId, LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
+    private static final Sort SORT_BY_DATETIME = Sort.by(Sort.Direction.DESC, "dateTime");
 
     private final CrudMealRepository crudRepository;
 
@@ -23,21 +25,26 @@ public class DataJpaMealRepository implements MealRepository {
 
     @Override
     public boolean delete(int id, int userId) {
+        Meal meal = get(id, userId);
+        if (meal != null) {
+            crudRepository.delete(meal);
+            return true;
+        }
         return false;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.findMealByIdAndUserId(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findMealsByUserId(SORT_BY_DATETIME, userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.findMealsByUserIdAndDateTimeAfterAndDateTimeBefore(SORT_BY_DATETIME, userId, startDateTime, endDateTime);
     }
 }
