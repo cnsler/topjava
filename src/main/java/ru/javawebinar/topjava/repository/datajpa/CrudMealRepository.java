15da48618420d5e77a902961577b33a2c21b89e0
cnsler <cnsler@gmail.com>
2022-11-08 17:44:54 +0200

    HW5 implement all JpaRepository methods

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -2,6 +2,10 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
@@ -9,9 +13,21 @@ import java.util.List;
 
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
 
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
     Meal findMealByIdAndUserId(int id, int userId);
 
     List<Meal> findMealsByUserId(Sort sort, int userId);
 
-    List<Meal> findMealsByUserIdAndDateTimeAfterAndDateTimeBefore(Sort sort, int userId, LocalDateTime start, LocalDateTime end);
+    @Query("""
+                SELECT m FROM Meal m 
+                WHERE m.user.id=?3 AND m.dateTime >= ?1 AND m.dateTime < ?2
+                ORDER BY m.dateTime DESC
+            """)
+    List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime,
+                                  LocalDateTime endDateTime,
+                                  int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
@@ -12,39 +13,41 @@ import java.util.List;
 public class DataJpaMealRepository implements MealRepository {
     private static final Sort SORT_BY_DATETIME = Sort.by(Sort.Direction.DESC, "dateTime");
 
-    private final CrudMealRepository crudRepository;
+    private final CrudMealRepository mealRepository;
+    private final CrudUserRepository userRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
-        this.crudRepository = crudRepository;
+    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository userRepository) {
+        this.mealRepository = crudRepository;
+        this.userRepository = userRepository;
     }
 
+    @Transactional
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (!meal.isNew() && get(meal.id(), userId) == null) {
+            return null;
+        }
+        meal.setUser(userRepository.getReferenceById(userId));
+        return mealRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        Meal meal = get(id, userId);
-        if (meal != null) {
-            crudRepository.delete(meal);
-            return true;
-        }
-        return false;
+        return mealRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return crudRepository.findMealByIdAndUserId(id, userId);
+        return mealRepository.findMealByIdAndUserId(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return crudRepository.findMealsByUserId(SORT_BY_DATETIME, userId);
+        return mealRepository.findMealsByUserId(SORT_BY_DATETIME, userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return crudRepository.findMealsByUserIdAndDateTimeAfterAndDateTimeBefore(SORT_BY_DATETIME, userId, startDateTime, endDateTime);
+        return mealRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
     }
 }
