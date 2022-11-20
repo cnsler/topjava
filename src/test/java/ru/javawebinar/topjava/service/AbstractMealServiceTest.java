659f89ad96875f93d4b4c343227d6529708c28f5
cnsler <cnsler@gmail.com>
2022-11-20 14:51:34 +0200

    HW6 replace deprecated method in JdbcMealServiceTest

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -110,7 +110,7 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Test
     public void createWithException() throws Exception {
-        Assume.assumeFalse(environment.acceptsProfiles(Profiles.JDBC));
+        Assume.assumeFalse(environment.acceptsProfiles(org.springframework.core.env.Profiles.of(Profiles.JDBC)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "  ", 300), USER_ID));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, null, "Description", 300), USER_ID));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 9), USER_ID));
