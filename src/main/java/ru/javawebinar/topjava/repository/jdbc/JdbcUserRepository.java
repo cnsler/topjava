94ae58001fa9ac64db964731d90795a86ddda2f0
cnsler <cnsler@gmail.com>
2022-11-23 21:45:09 +0200

    HW6 refactor user extraction after review

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.repository.jdbc;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -11,19 +12,40 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.util.UserExtractor;
 import ru.javawebinar.topjava.util.ValidationUtil;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Repository
 @Transactional(readOnly = true)
 public class JdbcUserRepository implements UserRepository {
 
-    private static final UserExtractor userExtractor = new UserExtractor();
+    private static final ResultSetExtractor<List<User>> USER_EXTRACTOR = rs -> {
+        Map<Integer, User> result = new LinkedHashMap<>();
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            User user = result.get(id);
+            if (user == null) {
+                User newUser = new User(id,
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getInt("calories_per_day"),
+                        rs.getBoolean("enabled"),
+                        rs.getDate("registered"),
+                        new HashSet<>());
+                user = newUser;
+                result.computeIfAbsent(id, s -> newUser);
+            }
+            String role = rs.getString("role");
+            if (role != null && !role.isEmpty()) {
+                user.getRoles().add(Role.valueOf(role));
+            }
+        }
+        return new ArrayList<>(result.values());
+    };
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -76,7 +98,7 @@ public class JdbcUserRepository implements UserRepository {
                 SELECT * FROM users u 
                 LEFT JOIN user_roles ur on u.id = ur.user_id 
                 WHERE id=?
-                """, userExtractor, id);
+                """, USER_EXTRACTOR, id);
         return DataAccessUtils.singleResult(users);
     }
 
@@ -86,7 +108,7 @@ public class JdbcUserRepository implements UserRepository {
                 SELECT * FROM users u 
                 LEFT JOIN user_roles ur on u.id = ur.user_id 
                 WHERE email=?
-                """, userExtractor, email);
+                """, USER_EXTRACTOR, email);
         return DataAccessUtils.singleResult(users);
     }
 
@@ -96,7 +118,7 @@ public class JdbcUserRepository implements UserRepository {
                 SELECT * FROM users u 
                 LEFT JOIN user_roles ur on u.id = ur.user_id 
                 ORDER BY name, email
-                """, userExtractor);
+                """, USER_EXTRACTOR);
     }
 
     private void addRoles(User user) {
