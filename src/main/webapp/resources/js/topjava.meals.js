da652508f07461b9143bee06053c546e622aff58
cnsler <cnsler@gmail.com>
2022-12-13 21:00:45 +0200

    HW9 add DateTimePicker to filter and modal

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -18,6 +18,25 @@ function clearFilter() {
 }
 
 $(function () {
+    $("#dateTime").datetimepicker({
+        format: 'Y-m-d H:m'
+    });
+    $("#startDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d'
+    });
+    $("#endDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d'
+    });
+    $('#startTime').datetimepicker({
+        datepicker: false,
+        format: 'H:i'
+    });
+    $('#endTime').datetimepicker({
+        datepicker: false,
+        format: 'H:i'
+    });
     makeEditable(
         $("#datatable").DataTable({
             "ajax": {
