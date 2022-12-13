ac5312c27bef9ad30e46d58c4c07bb3f632340fc
cnsler <cnsler@gmail.com>
2022-12-13 22:19:19 +0200

    HW9 add min/max Date/Time to filter

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -24,19 +24,39 @@ $(function () {
     });
     $("#startDate").datetimepicker({
         timepicker: false,
-        format: 'Y-m-d'
+        format: 'Y-m-d',
+        onShow:function( ct ) {
+            this.setOptions({
+                maxDate: $("#endDate").val() ? $("#endDate").val() : false
+            })
+        }
     });
     $("#endDate").datetimepicker({
         timepicker: false,
-        format: 'Y-m-d'
+        format: 'Y-m-d',
+        onShow:function( ct ) {
+            this.setOptions({
+                minDate: $("#startDate").val() ? $("#startDate").val() : false
+            })
+        }
     });
     $('#startTime').datetimepicker({
         datepicker: false,
-        format: 'H:i'
+        format: 'H:i',
+        onShow:function( ct ) {
+            this.setOptions({
+                maxTime: $('#endTime').val() ? $('#endTime').val() : false
+            })
+        }
     });
     $('#endTime').datetimepicker({
         datepicker: false,
-        format: 'H:i'
+        format: 'H:i',
+        onShow:function( ct ) {
+            this.setOptions({
+                minTime: $('#startTime').val() ? $('#startTime').val() : false
+            })
+        }
     });
     makeEditable(
         $("#datatable").DataTable({
