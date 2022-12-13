0735d1e8a81165f5d59db583c64d927bc171ca50
cnsler <cnsler@gmail.com>
2022-12-13 17:56:52 +0200

    HW9 fix java.lang.NumberFormatException
    failed to convert calories of type String to type int

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -90,7 +90,7 @@ public class Meal extends AbstractBaseEntity {
         this.description = description;
     }
 
-    public void setCalories(int calories) {
+    public void setCalories(Integer calories) {
         this.calories = calories;
     }
 
