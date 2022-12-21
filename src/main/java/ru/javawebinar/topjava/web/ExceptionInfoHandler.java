34201d756adaa5f016f7699361f13d401685b18f
cnsler <cnsler@gmail.com>
2022-12-22 00:55:12 +0200

    HW10 add user email duplication exception message internalization

--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -2,6 +2,9 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -31,6 +34,9 @@ import static ru.javawebinar.topjava.util.exception.ErrorType.*;
 public class ExceptionInfoHandler {
     private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
 
+    @Autowired
+    private MessageSource messageSource;
+
     //  http://stackoverflow.com/a/22358422/548473
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(NotFoundException.class)
@@ -42,7 +48,8 @@ public class ExceptionInfoHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
         if (e.getMessage().contains("users_unique_email_idx")) {
-            return new ErrorInfo(req.getRequestURL(), VALIDATION_ERROR, List.of("User with this email already exists"));
+            return new ErrorInfo(req.getRequestURL(), VALIDATION_ERROR,
+                    List.of(messageSource.getMessage("user.unique", null, LocaleContextHolder.getLocale())));
         }
         return logAndGetErrorInfo(req, e, true, DATA_ERROR);
     }
